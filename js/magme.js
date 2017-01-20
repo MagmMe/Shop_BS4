09277fcc7cd257ad9708093b8111d46b15046eda
MagmMe <marcin@magm.me>
2017-01-20 01:26:48 +0100

    Order rules site completed

--- js/magme.js
+++ js/magme.js
@@ -17,12 +17,47 @@
  //        });
 
 
-	 	// Magme Hero Smooth Scroll
+	 	// Magme Hero Smooth Scroll and Regulamin Site smooth scroll
 		$('#start').on('click', function(e){
 		    e.preventDefault();
 		    var target= $('#end');
 		    $('html, body').stop().animate({
 		       scrollTop: target.offset().top
+		    }, 1000);
+		});
+		$('#regOne').on('click', function(e){
+		    e.preventDefault();
+		    var target= $('#reg1');
+		    $('html, body').stop().animate({
+		       scrollTop: target.offset().top - 200
+		    }, 1000);
+		});
+		$('#regTwo').on('click', function(e){
+		    e.preventDefault();
+		    var target= $('#reg2');
+		    $('html, body').stop().animate({
+		       scrollTop: target.offset().top - 200
+		    }, 1000);
+		});
+		$('#regThree').on('click', function(e){
+		    e.preventDefault();
+		    var target= $('#reg3');
+		    $('html, body').stop().animate({
+		       scrollTop: target.offset().top - 200
+		    }, 1000);
+		});
+		$('#regFour').on('click', function(e){
+		    e.preventDefault();
+		    var target= $('#reg4');
+		    $('html, body').stop().animate({
+		       scrollTop: target.offset().top - 200
+		    }, 1000);
+		});
+		$('#regFive').on('click', function(e){
+		    e.preventDefault();
+		    var target= $('#reg5');
+		    $('html, body').stop().animate({
+		       scrollTop: target.offset().top - 200
 		    }, 1000);
 		});
 
